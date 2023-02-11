8e79d6da6902f1acf447c197291ae5e4a6c93f91
Dabeer Shaikh <dab8106@gmail.com>
2023-02-11 15:29:21 +0530

    Added a visitors counter to the application

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+counter = 0
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    global counter
+    counter += 1
+    return "Hello, World! This page has been visited {} times.".format(counter)
 
 if __name__ == "__main__":
     app.run(debug=True)
